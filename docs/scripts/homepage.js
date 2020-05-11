7e92c17d3e498a02bd009c11910cf637d5d204f6
JerrellN <nolen.jerrell@hotmail.com>
2020-05-11 13:35:41 -0500

    Fixed Modal images

--- docs/scripts/homepage.js
+++ docs/scripts/homepage.js
@@ -10,16 +10,151 @@ $(document).ready(function(){
 })
 //Need help here ^
 
-function showModal(elem)
+function showModalMG()
 {
     var x = document.getElementById("myModal");
-    var y = document.getElementById("aboutBody")
+    var y = document.getElementById("aboutBody");
+    document.getElementById("MTP").src = document.getElementById("mikalPic").src;
+    
+
     if (x.style.display === "none")
     {
         x.style.display = "block";
         y.scroll = "no";
         y.style.overflow = hidden;
-        x.setAttribute("src", elem.src)
+        
+    }
+    else
+    {
+        x.style.display = "none";
+        y.scroll = "yes";
+        y.scroll.overflow = scroll;
+    }
+}
+
+function showModalJN()
+{
+    var x = document.getElementById("myModal");
+    var y = document.getElementById("aboutBody");
+    document.getElementById("MTP").src = document.getElementById("jerrellPic").src;
+    
+
+    if (x.style.display === "none")
+    {
+        x.style.display = "block";
+        y.scroll = "no";
+        y.style.overflow = hidden;
+        
+    }
+    else
+    {
+        x.style.display = "none";
+        y.scroll = "yes";
+        y.scroll.overflow = scroll;
+    }
+}
+
+function showModalDD()
+{
+    var x = document.getElementById("myModal");
+    var y = document.getElementById("aboutBody");
+    document.getElementById("MTP").src = document.getElementById("dezoPic").src;
+    
+
+    if (x.style.display === "none")
+    {
+        x.style.display = "block";
+        y.scroll = "no";
+        y.style.overflow = hidden;
+        
+    }
+    else
+    {
+        x.style.display = "none";
+        y.scroll = "yes";
+        y.scroll.overflow = scroll;
+    }
+}
+
+function showModalMM()
+{
+    var x = document.getElementById("myModal");
+    var y = document.getElementById("aboutBody");
+    document.getElementById("MTP").src = document.getElementById("tinoPic").src;
+    
+
+    if (x.style.display === "none")
+    {
+        x.style.display = "block";
+        y.scroll = "no";
+        y.style.overflow = hidden;
+        
+    }
+    else
+    {
+        x.style.display = "none";
+        y.scroll = "yes";
+        y.scroll.overflow = scroll;
+    }
+}
+
+function showModalAB()
+{
+    var x = document.getElementById("myModal");
+    var y = document.getElementById("aboutBody");
+    document.getElementById("MTP").src = document.getElementById("antPic").src;
+    
+
+    if (x.style.display === "none")
+    {
+        x.style.display = "block";
+        y.scroll = "no";
+        y.style.overflow = hidden;
+        
+    }
+    else
+    {
+        x.style.display = "none";
+        y.scroll = "yes";
+        y.scroll.overflow = scroll;
+    }
+}
+
+function showModalAG()
+{
+    var x = document.getElementById("myModal");
+    var y = document.getElementById("aboutBody");
+    document.getElementById("MTP").src = document.getElementById("aidanPic").src;
+    
+
+    if (x.style.display === "none")
+    {
+        x.style.display = "block";
+        y.scroll = "no";
+        y.style.overflow = hidden;
+        
+    }
+    else
+    {
+        x.style.display = "none";
+        y.scroll = "yes";
+        y.scroll.overflow = scroll;
+    }
+}
+
+function showModalEK()
+{
+    var x = document.getElementById("myModal");
+    var y = document.getElementById("aboutBody");
+    document.getElementById("MTP").src = document.getElementById("emilyPic").src;
+    
+
+    if (x.style.display === "none")
+    {
+        x.style.display = "block";
+        y.scroll = "no";
+        y.style.overflow = hidden;
+        
     }
     else
     {
